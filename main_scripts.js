2c50a02f279c914591cc35ca8e3b6f22c7cf6e0c
SharronMSM <38895771+SharronMSM@users.noreply.github.com>
2020-09-24 18:30:10 -0400

    qc
    qc

---
+++ main_scripts.js
@@ -0,0 +1,10 @@
+// Show Main fixed mobile menu
+function showMfmm() {
+  document.getElementsByClassName('Mfmm')[0].style.display = "block";
+  document.getElementsByClassName('Mfmm')[0].style.overflowY = "none";
+}
+
+// Hide Main fixed mobile menu
+function hideMfmm() {
+  document.getElementsByClassName('Mfmm')[0].style.display = "none";
+}
