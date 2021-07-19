18041ff1da3cca8a827bfbe52c59afd4b5272318
SharronMSM <38895771+SharronMSM@users.noreply.github.com>
2021-07-19 01:50:11 -0400

    qc
    qc

---
+++ scripts/main_scripts.js
@@ -0,0 +1,31 @@
+// Show fixed mobile menu
+function showMfmm() {
+  document.getElementById('fixed-mobile-menu').style.display = "block";
+}
+
+// Hide Main fixed mobile menu
+function hideMfmm() {
+  document.getElementById('fixed-mobile-menu').style.display = "none";
+}
+
+function showMfapExtension() {
+  document.getElementsByClassName('mfap-ec')[0].style.display = "block";
+}
+
+function hideMfapExtension() {
+  document.getElementsByClassName('mfap-ec')[0].style.display = "none";
+}
+
+
+
+
+
+
+function showFixedSignupModal() {
+  document.getElementById('fixed-signup-modal').style.display = "block";
+  document.getElementById('fixed-signup-modal').style.display = "flex";
+}
+
+function hideFixedSignupModal() {
+  document.getElementById('fixed-signup-modal').style.display = "none";
+}
