e76241d704ad4ba22060b4c8e53f9ffae90d176e
SharronMSM <38895771+SharronMSM@users.noreply.github.com>
2021-11-02 23:48:43 -0400

    qc
    qc

---
+++ scripts/main-scripts.js
@@ -0,0 +1,22 @@
+// SHOW main fixed audio player _ extension
+function SHOWmainFixedAudioPlayer() {
+  document.getElementById("mfap-extension").style.display = "block";
+}
+
+
+// HIDE main fixed audio player _ extension
+function HIDEmainFixedAudioPlayer() {
+  document.getElementById("mfap-extension").style.display = "none";
+}
+
+
+// SHOW main fixed mobile menu _ sidebar
+function SHOWmainFixedMobileMenu() {
+  document.getElementById('mfmm-box').style.display = "block";
+}
+
+// HIDE main fixed mobile menu _ sidebar
+function HIDEmainFixedMobileMenu() {
+  document.getElementById('main-fixed-audio-player').style.display = none;
+
+}
